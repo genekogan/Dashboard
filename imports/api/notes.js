03bed7017c0043d3ee2d8b4f0414c07e918eb0ef
Gene Kogan <kogan.gene@gmail.com>
2018-03-25 16:35:18 +0200

    got rid of sticky note with markdown -- so now its not saved

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -7,22 +7,3 @@ export const Events = new Mongo.Collection('events');
 export const Travels = new Mongo.Collection('travels');
 
 
-
-if (Meteor.isServer) {
-	Meteor.publish('notes_loaded', function(){return Notes.find({})}, {is_auto: true});
-}
-
-if (Meteor.isClient) {
-  	Meteor.subscribe('notes_loaded', function(){
-     	if (Notes.find({list_id: null}).count() == 0) {
-     		Notes.insert({text:'', markdown:'hello world', list_id: null, order: 0, createdAt: new Date()});
-     	}
-
-
-     	console.log(Notes.find().count())
-     	console.log(Notes.find().forEach(function (n) {
-     		console.log(n);
-     	}))
-
-	});
-}
